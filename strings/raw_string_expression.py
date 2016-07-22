a0127f438a579604e4d0f4d2e733875e5b497b6a
tn-746 <swetha.irukulapati@gmail.com>
2016-07-22 17:31:31 +0530

    new file

---
+++ strings/raw_string_expression.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+#raw expression
+#python test.py
+
+print "without raw expression printing of " r'c:\\test'
+
+#prints c:\test
+print 'c:\\test'
+
+print "with raw expression printing of " r'c:\\test'
+
+#prints c:\\test
+print r'c:\\test'
