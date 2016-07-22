77a3076538292f06a095c86cd9a19f154b505d00
tn-746 <swetha.irukulapati@gmail.com>
2016-07-22 17:14:05 +0530

    triple quotes in python

---
+++ strings/triple-quotes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+#Triple Quotes
+#python test.py
+
+#string with multiple lines
+string = """python triple quotes
+several lines with triple quotes
+non printable characters such as TAB (\t)
+Newline character, \n new line
+string variable string"""
+
+#printing string
+print string
